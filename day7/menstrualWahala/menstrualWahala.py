dd959b0875d7b7a8b448136a543ee9d532b55e5d
Mercy <sheroudensi@gmail.com>
2024-05-13 15:00:58 +0100

    menstrualWahala

---
+++ day7/menstrualWahala/menstrualWahala.py
@@ -0,0 +1,14 @@
+import datetime
+
+def main():
+	print("FLOW-APP")
+	LMP= input("First Date of Last Menstrual Period")
+	cycle_length = input("Cycle Length(dd/mm/yyyy)")
+
+	LMP = datetime.datetime.strptime(LMP, "%d/%m/%Y")
+	cycle = int(cycle)
+	estimated_ovulation = cycle - 14
+	safe_period = 
+	ovulation_day = datetime.timedelta(first, days = estimated_ovulation_day)
+	evaluation_date = LMP + ovulation_day
+p
