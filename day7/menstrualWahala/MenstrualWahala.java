4e878efecbf2f5713ddb393235b2d62d95fca1d0
Mercy <sheroudensi@gmail.com>
2024-05-13 17:01:06 +0100

    menstrualWahala

---
+++ day7/menstrualWahala/MenstrualWahala.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+public class MenstrualWahala {
+	
+		public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter date of last menstrual period: ");
+		String lastMenstrualPeriod = input.nextLine();
+		System.out.println("Enter the cycle length: ");
+		int cycleLength = input.nextInt();
+		System.out.println(ovulationDay(cycleLength));
+		System.out.println(lastPeriod(lastMenstrualPeriod));
+		}
+
+	public static String lastPeriod(String lastMenstrualPeriod){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("[dd/MM/yyyy][dd-MM-yyyy][dd/MMM/yyyy]");				LocalDate localDate = LocalDate.parse(lastMenstrualPeriod, formatter);
+		LocalDate currentDate =
+}
+
+		public static int ovulationDay(int cycleLength) {
+			estimatedOvulation = cycleLength - 14;
+	
+			
+			
+
+}
+		
+	}
--- day7/menstrualWahala/menstrualWahala.py
+++ day7/menstrualWahala/menstrualWahala.py
@@ -1,14 +1,18 @@
-import datetime
+from datetime import datetime
 
 def main():
-	print("FLOW-APP")
-	LMP= input("First Date of Last Menstrual Period")
+	print("MENSTRUAL WAHALA")
+	lmp= input("First Date of Last Menstrual Period")
 	cycle_length = input("Cycle Length(dd/mm/yyyy)")
 
-	LMP = datetime.datetime.strptime(LMP, "%d/%m/%Y")
-	cycle = int(cycle)
+	lmp = datetime.datetime(lmp, "%d/%m/%Y")
+	cycle = int(cycle_length)
 	estimated_ovulation = cycle - 14
-	safe_period = 
-	ovulation_day = datetime.timedelta(first, days = estimated_ovulation_day)
-	evaluation_date = LMP + ovulation_day
-p
+	 
+	ovulation_day = datetime.timedelta(days = estimated_ovulation)
+	ovulation_date = lmp + ovulation_day
+	print("Ovulation Date" + ovulation_date.strftime("%d/%m/%Y"))
+
+
+
+
